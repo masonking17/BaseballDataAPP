a9562ad69428dbbea8d60eeb4f9c4b7ffdf3ef2c
Mason J. King <mason.king@centre.edu>
2019-10-29 21:21:11 -0400

    created table and linked all files togther

--- BaseballDataAPP.js
+++ BaseballDataAPP.js
@@ -1,1 +1,34 @@
+var dataPromise = d3.json("teams.json")
 
+dataPromise.then(
+
+function(dataPromise)
+    {
+        maketable(dataPromise);
+        console.log("good", (dataPromise))
+        
+        
+    },
+
+function(error)
+    {
+        console.log ("error", (error))
+}
+       
+)
+
+
+
+
+var maketable = function(dataPromise)
+{
+  d3.select("body")  
+    .append("table")
+    .attr("id", "MLBTeams")
+    d3.select("#MLBTeams")
+    .append("body")
+    .selectAll("tr")
+    .data(dataPromise)
+    .enter()
+    .append("tr")    
+}
