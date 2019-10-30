ae5f71844d9fc160ddc953a6a4c34836b5c61ecb
Mason J. King <mason.king@centre.edu>
2019-10-30 19:51:31 -0400

    updated table info

--- BaseballDataAPP.js
+++ BaseballDataAPP.js
@@ -5,7 +5,7 @@ dataPromise.then(
 function(dataPromise)
     {
         maketable(dataPromise);
-        console.log("good", (dataPromise))
+        console.log("good", dataPromise)
         
         
     },
@@ -22,7 +22,7 @@ function(error)
 
 var maketable = function(dataPromise)
 {
-  d3.select("body")  
+  d3.select("#tableTitle")  
     .append("table")
     .attr("id", "MLBTeams")
     d3.select("#MLBTeams")
@@ -30,5 +30,39 @@ var maketable = function(dataPromise)
     .selectAll("tr")
     .data(dataPromise)
     .enter()
-    .append("tr")    
+    .append("tr")
+    .attr("id", function(d){return d.team_id})
+    .text(function(d){return d.team_id})
+     .on("click", function(d) {displayInfo(d)}) // {console.log(d.abbreviation) 
+    // d3.select("#"+ d.teamID)
+
 }
+var displayInfo = function(info)
+{
+    var box= 
+    d3.select("#teamInfo");
+    
+     box.append("p")
+    .text("Team Abbreviation: " + info.abbreviation)
+    
+    box.append("p")
+    .text("Active Franchise?: " + info.active)
+    
+    box.append("p")
+    .text("City: " + info.city)
+    
+     box.append("p")
+    .text("Conference: " + info.conference)
+    
+     box.append("p")
+    .text("Division: " + info.division)
+    
+     box.append("p")
+    .text("Stadium Name: " + info.site_name)
+    
+     box.append("p")
+    .text("State: " + info.state)
+
+}
+          
+
